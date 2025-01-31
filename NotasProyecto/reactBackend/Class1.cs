eb6b9561f155a62eb921786e169ad0871b58927a
Elizabeth Abrego <an0174032023@unab.edu.sv>
2025-01-31 10:00:32 -0600

    Guía 2: Uso de Entity Framework. Completada.

---
+++ NotasProyecto/reactBackend/Class1.cs
@@ -0,0 +1,11 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace reactBackend
+{
+    public class Class1
+    {
+
+        //Data Source = LAPTOP - GK1DJVHC\SQLEXPRESS;Initial Catalog = registroAlumno; User ID = Sonia; Encrypt=False
+
+    }
+
+}
