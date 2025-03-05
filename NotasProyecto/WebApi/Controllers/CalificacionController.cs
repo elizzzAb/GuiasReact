fc91e11dcc0ab4488bb4d18f1c5dcbb380134115
Elizabeth Abrego <an0174032023@unab.edu.sv>
2025-03-05 07:22:42 -0600

    Guía 12: EndPoint para calificaciones. Completada.

---
+++ NotasProyecto/WebApi/Controllers/CalificacionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using reactBackend.Models;
+using reactBackend.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalificacionController : ControllerBase
+    {
+        private CalificacionDao _cdao = new CalificacionDao();
+
+        #region Lista de Calificaciones
+        [HttpGet("calificaciones")]
+        public List<Calificacion> get(int idMatricula)
+        {
+            // Invocando al metodo CalificacionDao
+            return _cdao.seleccion(idMatricula);
+        }
+
+        #endregion 
+    }
+}
---
+++ NotasProyecto/reactBackend/Repository/CalificacionDao.cs
@@ -0,0 +1,47 @@
+﻿using reactBackend.Context;
+using reactBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reactBackend.Repository
+{
+    public class CalificacionDao
+    {
+        private RegistroAlumnoContext _contexto = new RegistroAlumnoContext();
+
+        #region Seleccionar_lista_caificaciones 
+        public List<Calificacion> seleccion(int matriculaid)
+        {
+
+            var matricula = _contexto.Matriculas.Where(x => x.Id == matriculaid);
+            Console.WriteLine("matricula encontrada");
+            try
+            {
+                if (matricula != null)
+                {
+
+                    var calificacion = _contexto.Calificacions.Where(x => x.Id == matriculaid).ToList();
+                    return calificacion;
+                }
+                else
+                {
+
+                    return null;
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+
+
+        }
+        #endregion
+    }
+}
