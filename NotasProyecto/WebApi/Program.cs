0a94cb64ef56ded8015e5fa2cb315277ade75d5b
Elizabeth Abrego <an0174032023@unab.edu.sv>
2025-03-08 10:18:49 -0600

    Guia 15: Solucionar el error del Bloqueo Cors (Dominio cruzado). Completada.

--- NotasProyecto/WebApi/Program.cs
+++ NotasProyecto/WebApi/Program.cs
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(policyBuilder => policyBuilder.AddDefaultPolicy(
+    policy => policy.WithOrigins("*").AllowAnyHeader().AllowAnyMethod()
+    ));
 
 var app = builder.Build();
 
@@ -16,6 +19,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
